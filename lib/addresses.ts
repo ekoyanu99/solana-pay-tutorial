ce2dc7ba6311851a8db056f7e0871285119ab3b3
Eko Yanuarso Budi <36530602+ekoyanu99@users.noreply.github.com>
2022-04-06 13:57:41 +0700

    commit

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -1,4 +1,8 @@
 import { PublicKey } from "@solana/web3.js"
 
 // Your shop wallet address
-export const shopAddress = new PublicKey('...') 
+export const shopAddress = new PublicKey('8uq8kWscA3NrccHHHaQjM8EeEHPqNHnk7qAFUrqfLS6U') 
+// usdc sc devnet
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+
+export const couponAddress = new PublicKey('DsGruxTD5TJT94m6hZNp5j14nVygbi5T8gzy8aKntp9z')
